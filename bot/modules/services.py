5e1972613093c12a11c535422ec0c1f39b9c695b
Takeshi <takeshi.gi75@gmail.com>
2026-05-16 18:48:22 +0700

    feat: add Docker workflow and implement bot service commands for authentication and logging

--- bot/modules/services.py
+++ bot/modules/services.py
@@ -33,9 +33,9 @@ async def start(_, message):
     lang = Language()
     buttons = ButtonMaker()
     buttons.url_button(
-        lang.START_BUTTON1, "https://www.github.com/SilentDemonSD/WZML-X"
+        lang.START_BUTTON1, "https://github.com/takeshi7502/wzml"
     )
-    buttons.url_button(lang.START_BUTTON2, "https://t.me/WZML_X")
+    buttons.url_button(lang.START_BUTTON2, "https://t.me/takeshi_updrive")
     reply_markup = buttons.build_menu(2)
 
     if len(message.command) > 1 and message.command[1] == "wzmlx":
